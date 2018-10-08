500746e7e0f0068d8a1a28addcf84be3cab15bed
shivamGandhi <shivamgandhi95@gmail.com>
2018-10-08 08:59:50 -0400

    Added animation.

--- app/src/main/java/com/example/shivamgandhi/bouncingball/HomeActivity.java
+++ app/src/main/java/com/example/shivamgandhi/bouncingball/HomeActivity.java
@@ -2,12 +2,26 @@ package com.example.shivamgandhi.bouncingball;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 public class HomeActivity extends AppCompatActivity {
 
+    ImageView bouncinBallIv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+
+        bouncinBallIv = findViewById(R.id.imageView);
+
+        // Load the animation
+        Animation animSlide = AnimationUtils.loadAnimation(getApplicationContext(),
+                R.anim.slide);
+
+        // Start the animation
+        bouncinBallIv.startAnimation(animSlide);
     }
 }
